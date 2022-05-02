3b34bc3a52c5eacc9b9d10b15ef2760729b52f5c
howardlee79 <79288683+howardlee79@users.noreply.github.com>
2022-05-02 09:15:10 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,27 @@
-self.addEventListener('install', (e) => {
+var cacheName = 'hyosung-pwa';
+var filesToCache = [
+  '/hyosung/default.html',
+  '/hyosung/index.js',
+  '/hyosung/css/bootstrap.css',
+  '/hyosung/css/font-awesome.css',
+  '/hyosung/css/theme.css',
+];
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function(e) {
   e.waitUntil(
-    caches.open('fox-store').then((cache) => cache.addAll([
-      '/hyosung/default.html',
-      '/hyosung/index.js',
-      '/hyosung/css/bootstrap.css',
-      '/hyosung/css/font-awesome.css',
-      '/hyosung/css/theme.css',
-    ])),
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
   );
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', (e) => {
-  console.log(e.request.url);
+/* Serve cached content when offline */
+self.addEventListener('fetch', function(e) {
   e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request)),
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
   );
 });
